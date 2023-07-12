9fa1eb7ac04890d4141cb1f669a93c2e6fa7e969
danivodka5 <datmax1@hotmail.com>
2023-07-13 00:58:00 +0200

    LeetCode 3 v0.1

---
+++ Longest_Substring_Without_Repeating_Characters/src/Solucion.java
@@ -0,0 +1,16 @@
+
+// Hago un bucle para recorrer todo el String 
+// Almaceno las letras una a una, si encuentro una que ya he almacenado, la guardo en otro lugar.
+// El orden de almacenamiento depende de si se repite
+public class Solucion {
+	public static void main(String[] args) {
+		
+	String s = "abcabcabb";
+	
+	
+	
+	
+	
+	
+	}
+}
